ebb4d73b29d35c3c189827636ed4bc70c65be858
poyntesm <poyntesm@962f16a3-720d-0410-a273-e2d97f957140>
2006-03-20 14:50:22 +0000

    - Bug:0000001 -	Add support for MySQL5 (Round 2)   As guessed there was more..Install Garage.txt needed new edits. This will need to be checked via a vanilla install with EasyMod. - Bug:0000001 -	Add support for MySQL5 (Round 1)..there may be more to come. - New functions select_insurance_business_data & select_all_vehicle_data

--- root/includes/functions_garage.php
+++ root/includes/functions_garage.php
@@ -3665,6 +3665,86 @@ class garage_lib
 			return (FALSE);
 		}
 	}
+
+	/*========================================================================*/
+	// Select All Vehicles Data From Db
+	// Usage: select_all_vehicle_data();
+	/*========================================================================*/
+	function select_all_vehicle_data($additional_where, $order_by, $sort_order, $start, $end)
+	{
+		global $db;
+		//Select All Vehicles Information
+		$sql = "SELECT g.*, makes.make, models.model, user.username, count(mods.id) AS total_mods, count(*) as total
+        		FROM " . GARAGE_TABLE . " AS g 
+                    		LEFT JOIN " . GARAGE_MODS_TABLE . " AS mods ON mods.garage_id = g.id
+			        LEFT JOIN " . GARAGE_MAKES_TABLE . " AS makes ON g.make_id = makes.id 
+			        LEFT JOIN " . GARAGE_MODELS_TABLE . " AS models ON g.model_id = models.id 
+			        LEFT JOIN " . USERS_TABLE . " AS user ON g.member_id = user.user_id 
+			WHERE makes.pending = 0 AND models.pending = 0
+				".$search_data['where']."
+		        GROUP BY g.id
+			ORDER BY $order_by $sort_order";
+		if ( (!empty($start)) AND (!empty($end)) )
+		{
+			$sql .= "LIMIT $start, $end";
+		}
+
+      		if ( !($result = $db->sql_query($sql)) )
+      		{
+         		message_die(GENERAL_ERROR, 'Could Not Get Vehicle Data', '', __LINE__, __FILE__, $sql);
+      		}
+
+		while ($row = $db->sql_fetchrow($result) )
+		{
+			$rows[] = $row;
+		}
+		$db->sql_freeresult($result);
+
+		return $rows;
+	}
+
+	/*========================================================================*/
+	// Select All Insurance Data From Db
+	// Usage: select_all_vehicle_data();
+	/*========================================================================*/
+	function select_all_insurance_data($additional_where, $order_by, $sort_order, $start, $end)
+	{
+		global $db;
+		//Select All Vehicles Information
+		$sql = "SELECT i.*, g.*, b.title, b.id as business_id, makes.make, models.model, user.username, user.user_id,
+                        ( SUM(mods.price) + SUM(mods.install_price) ) AS total_spent,
+			CONCAT_WS(' ', g.made_year, makes.make, models.model) AS vehicle
+        		FROM " . GARAGE_INSURANCE_TABLE . " AS i 
+                    		LEFT JOIN " . GARAGE_TABLE . " AS g ON i.garage_id = g.id
+	                    	LEFT JOIN " . GARAGE_MODS_TABLE . " AS mods ON i.garage_id = mods.garage_id
+        	            	LEFT JOIN " . GARAGE_BUSINESS_TABLE . " AS b ON i.business_id = b.id
+			        LEFT JOIN " . GARAGE_MAKES_TABLE . " AS makes ON g.make_id = makes.id 
+		        	LEFT JOIN " . GARAGE_MODELS_TABLE . " AS models ON g.model_id = models.id 
+			        LEFT JOIN " . USERS_TABLE . " AS user ON g.member_id = user.user_id 
+			WHERE makes.pending = 0 AND models.pending = 0
+				".$search_data['where']."
+		        GROUP BY i.id
+			ORDER BY $order_by $sort_order";
+		if ( (!empty($start)) AND (!empty($end)) )
+		{
+			$sql .= "LIMIT $start, $end";
+		}
+
+      		if ( !($result = $db->sql_query($sql)) )
+      		{
+         		message_die(GENERAL_ERROR, 'Could Not Get Insurance Data', '', __LINE__, __FILE__, $sql);
+      		}
+
+		while ($row = $db->sql_fetchrow($result) )
+		{
+			$rows[] = $row;
+		}
+		$db->sql_freeresult($result);
+
+		return $rows;
+	}
+
+
 	
 	/*========================================================================*/
 	// Select All Vehicle Data From Db
@@ -3704,7 +3784,7 @@ class garage_lib
 		global $db;
 	
 		$sql = "SELECT mods.*, g.made_year, g.id, images.*, user.username, user.user_avatar_type, user.user_allowavatar, user.user_avatar, images.attach_ext, images.attach_id, images.attach_file, cats.title as category_title, makes.make, models.model, bus.title as business_name, ins.title as install_business_name, ins.id as install_business_id, CONCAT_WS(' ', g.made_year, makes.make, models.model) AS vehicle
-     			FROM " . GARAGE_MODS_TABLE . " AS mods, " . GARAGE_TABLE . " AS g 
+     			FROM (" . GARAGE_MODS_TABLE . " AS mods, " . GARAGE_TABLE . " AS g)
 				LEFT JOIN " . USERS_TABLE ." AS user ON g.member_id = user.user_id
 				LEFT JOIN " . GARAGE_CATEGORIES_TABLE . " AS cats ON cats.id = mods.category_id
         			LEFT JOIN " . GARAGE_IMAGES_TABLE . " AS images ON images.attach_id = mods.image_id 
@@ -3927,6 +4007,41 @@ class garage_lib
 		$db->sql_freeresult($result);
 
 		return $row;
+	}
+
+	/*========================================================================*/
+	// Select Model Data From DB
+	// Usage: select_model_data('model id');
+	/*========================================================================*/
+	function select_insurance_business_data($start, $where)
+	{
+		global $db, $where;
+
+		// Select Each Business
+      		$sql = "SELECT b.*, COUNT(DISTINCT b.id) as total
+       	 		FROM  " . GARAGE_BUSINESS_TABLE . " b 
+       			WHERE b.insurance = 1 
+				AND b.pending = 0
+				$where
+			GROUP BY b.id";
+		if (!empty($start))
+		{
+			$sql .=	"LIMIT $start, 25";
+		}
+
+      		if ( !($result = $db->sql_query($sql)) )
+      		{
+         		message_die(GENERAL_ERROR, 'Could Select Business Data', '', __LINE__, __FILE__, $sql);
+      		}
+
+		while( $row = $db->sql_fetchrow($result) )
+		{
+			$rows[] = $row;
+		}
+      		$db->sql_freeresult($result);
+
+		return $rows;
+
 	}
 
 	/*========================================================================*/
